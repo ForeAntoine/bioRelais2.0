bcc9c013d3713e46a8dee5f6d3917dfdc466a167
Antoine FORE <afore@geiffel.fr>
2017-12-14 09:48:44 +0100

    initial

---
+++ modeles/dao/dao.php
@@ -0,0 +1,8 @@
+<?php
+include 'DBConnex.php';
+include 'ResponsableDAO.php';
+include 'ProducteurDAO.php';
+include 'AdherentDAO.php';
+include 'PersonneDAO.php';
+
+ ?>
---
+++ vues/Accueil.php
@@ -0,0 +1,18 @@
+<div id="conteneur">
+
+    <div id='header'>
+    	<?php include 'haut.php' ;?>
+    </div>
+    
+    <div id="content">
+        <div class="texteAccueil">
+            <h1>BioRelai</h1>
+           
+    	</div>
+    	
+    </div>
+    	<div class="bas">
+        	<?php  include 'bas.php' ;?>
+    	</div>
+
+</div>
